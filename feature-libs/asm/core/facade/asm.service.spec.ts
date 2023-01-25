d6648bcafd8997394ce737fbe54ee29c3995d507
Lee <justin.lee01@sap.com>
2023-01-25 16:40:43 -0500

    Updating unit tests

--- feature-libs/asm/core/facade/asm.service.spec.ts
+++ feature-libs/asm/core/facade/asm.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { Store, StoreModule } from '@ngrx/store';
 import {
-  AsmCustomer360Response,
   AsmUi,
   CustomerSearchOptions,
   CustomerSearchPage,
@@ -104,41 +103,5 @@ describe('AsmService', () => {
       .subscribe((value) => (result = value))
       .unsubscribe();
     expect(result).toEqual(asmUi);
-  });
-
-  it('should dispatch an action to fetch customer 360 data', () => {
-    spyOn(store, 'dispatch').and.stub();
-
-    service.fetchCustomer360Data({
-      queries: [],
-      options: {
-        userId: '',
-      },
-    });
-
-    expect(store.dispatch).toHaveBeenCalledWith(
-      new AsmActions.Customer360Get({
-        queries: [],
-        options: {
-          userId: '',
-        },
-      })
-    );
-  });
-
-  it('should return customer 360 data', (done) => {
-    const customer360Response: AsmCustomer360Response = {
-      value: [],
-    };
-
-    store.dispatch(new AsmActions.Customer360GetSuccess(customer360Response));
-
-    service
-      .getCustomer360Data()
-      .pipe(take(1))
-      .subscribe((value) => {
-        expect(value).toEqual(customer360Response);
-        done();
-      });
   });
 });
